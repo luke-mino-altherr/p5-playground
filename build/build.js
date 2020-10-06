aab255b1095d023f278a9b879301068b5134e1ab
Luke Mino-Altherr <lminoaltherr@gmail.com>
2020-10-05 22:17:14 -0400

    Initial build of p5 with typescript and node.

---
+++ build/build.js
@@ -0,0 +1,24 @@
+var s = function (p) {
+    var width = document.querySelector('body').clientWidth;
+    var height = document.querySelector('body').clientHeight;
+    var margin_denominator = 4;
+    var marginx = width / margin_denominator;
+    var marginy = height / margin_denominator;
+    var rand_rect_size = p.random() * 100;
+    var x = width * (margin_denominator - 2) / margin_denominator - rand_rect_size;
+    var y = height * (margin_denominator - 2) / margin_denominator - rand_rect_size;
+    var randx = p.random() * x;
+    var randy = p.random() * y;
+    p.setup = function () {
+        p.createCanvas(width, height);
+        p.background(0);
+    };
+    p.draw = function () {
+        p.fill(255);
+        randx = p.random() * x;
+        randy = p.random() * y;
+        p.rect(randx + marginx, randy + marginy, rand_rect_size, rand_rect_size);
+    };
+};
+var myp5 = new p5(s);
+//# sourceMappingURL=../src/drawings/src/drawings/build.js.map
---
+++ src/drawings/index.ts
@@ -0,0 +1,30 @@
+const s = (p: any) => {
+  var width = document.querySelector('body').clientWidth;
+  var height = document.querySelector('body').clientHeight;
+
+  const margin_denominator = 4;
+
+  var marginx = width / margin_denominator;
+  var marginy = height / margin_denominator;
+
+  var rand_rect_size = p.random() * 100;
+
+  var x = width * (margin_denominator - 2) / margin_denominator - rand_rect_size;
+  var y = height * (margin_denominator - 2) / margin_denominator - rand_rect_size;
+
+  var randx = p.random() * x;
+  var randy = p.random() * y;
+
+  p.setup = function () {
+    p.createCanvas(width, height);
+    p.background(0);
+  };
+  p.draw = function () {
+    p.fill(255);
+    randx = p.random() * x;
+    randy = p.random() * y;
+    p.rect(randx + marginx, randy + marginy, rand_rect_size, rand_rect_size);
+  };
+};
+
+let myp5 = new p5(s);
